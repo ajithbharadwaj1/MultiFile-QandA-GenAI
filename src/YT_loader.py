2759eb5d337bc2e2d27f5629b31bf07d747aa0b3
ajithbharadwaj1 <ajithbharadwaj74@gmail.com>
2024-03-18 11:11:56 +0530

    Added latest PDF Query Files

--- src/YT_loader.py
+++
@@ -1,9 +0,0 @@
-import os
-import openai
-import sys
-sys.path.append('../..')
-
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
-
-hf_token = os.environ['hf_token']
